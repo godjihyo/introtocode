f50a9d99363c3beb1abbe0376e8870297ea9d1a1
Adara Pullum <apullum@southcityprep.org>
2018-06-19 18:21:46 +0000

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# introtocode
+# introtoco
---
+++ lists.py
@@ -0,0 +1,12 @@
+myshopping_list = ["Rope","Paint","duct tape"]
+for name in myshopping_list:
+    print (myshopping_list)
+    
+myshopping_list.append("shovel")
+print(myshopping_list)
+
+career_list = ["Theapist","Nurse","Bee Keeper"]
+print(career_list)
+
+career_list.append("Assaisin")
+print(career_list)
